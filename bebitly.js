dddd7566fe065506ec144efbeedf60788d66b0bd
Mike Dirolf <mike@10gen.com>
2010-07-14 21:45:25 -0400

    more stuff

--- bebitly.js
+++ bebitly.js
@@ -2,12 +2,18 @@ var urls = ["http://dirolf.com",
             "http://www.mongodb.org",
             "http://loudonvillelongboards.com",
             "http://www.google.com",
-            "http://bit.ly"];
+            "http://bit.ly",
+            "http://bsonspec.org",
+            "http://www.10gen.com",
+            "http://www.example.net"];
 var hashes = [];
 var shorts = {};
 var longs = {};
 var current_url;
 var current_hash;
+var start = new Date();
+var task_start = new Date();
+var score;
 
 function opt(url) {
     return "<a href='#' onclick='javascript:handle_long(\"" + url + "\")'>" + url + "</a><br>";
@@ -50,6 +56,7 @@ function shorten () {
 }
 
 function task () {
+    task_start = new Date();
     var r = Math.random();
     if (hashes.length === 0 || r < 0.5) {
         shorten();
@@ -58,12 +65,22 @@ function task () {
     }
 }
 
+function added_short () {
+    $("#total").html(hashes.length);
+    var sum = 0.0;
+    hashes.forEach(function (h) {
+                       sum += h.length;
+                   });
+    $("#average").html((sum / hashes.length).toFixed(2));
+}
+
 function handle_short (text) {
     if (!shorts[current_url]) {
         if (!longs[text]) {
             hashes.push(text);
             shorts[current_url] = text;
             longs[text] = current_url;
+            added_short();
             task();
         } else {
             alert("repeat hash!");
@@ -101,8 +118,23 @@ function handle_text (e) {
     return true;
 }
 
+function pad(s) {
+    s = s.toString();
+    return (s.length === 1) ? "0" + s : s;
+}
+
+function timers () {
+    var now = new Date();
+    var total = new Date(now.getTime() - start.getTime());
+    var task = new Date(now.getTime() - task_start.getTime());
+    $("#total_time").html(total.getMinutes() + ":" + pad(total.getSeconds()));
+    $("#task_time").html(task.getMinutes() + ":" + pad(task.getSeconds()));
+    setTimeout('timers()', 500);
+}
+
 function init () {
     $("#text").keyup(handle_text);
+    setTimeout('timers()', 500);
     task();
 }
 
